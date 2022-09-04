5bd542dff76fb5936a12053131e6cfac8805a2e3
David Perez <daperezcruz@gmail.com>
2022-09-03 22:44:06 -0500

    fixing comments

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ int main(int ac, char **av)
 			free(token);
 			continue;
 		}
-		if (token[0][0] == '#' || strcmp(token[0], "nop") == 0)
+		if (strcmp(token[0], "#") == 0 || strcmp(token[0], "nop") == 0)
 		{
 			free_token(token);
 			continue;
